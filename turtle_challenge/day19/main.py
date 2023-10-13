4a4c421e962c24a261f3fdfb2255bbfa8e7a1585
Piotr Karmelita <piotr@karmelita.eu>
2023-10-13 14:22:37 +0200

    turtle race

--- turtle_challenge/day19/main.py
+++ turtle_challenge/day19/main.py
@@ -1,33 +1,17 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color: ')
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-
-def move_forwards():
-    tim.forward(10)
-
-def move_backwards():
-    tim.backward(10)
-
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
-
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
-
-def clear():
-    tim.clear()
+for turtle_index in range(0, 6):
+    tim = Turtle(shape='turtle')
+    tim.color(colors[turtle_index])
     tim.penup()
-    tim.home()
-    tim.pendown()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
-screen.listen()
-screen.onkey(move_forwards, 'w')
-screen.onkey(move_backwards, 's')
-screen.onkey(turn_left, 'a')
-screen.onkey(turn_right, 'd')
-screen.onkey(clear, 'c')
+
 screen.exitonclick()
