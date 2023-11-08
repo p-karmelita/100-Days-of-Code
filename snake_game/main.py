25a2d98114d8e734117fcf2f781515046ab0f166
Piotr Karmelita <piotr@karmelita.eu>
2023-11-08 23:47:31 +0100

    snake class

--- snake_game/main.py
+++ snake_game/main.py
@@ -3,13 +3,19 @@ from turtle import Screen, Turtle
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('Snake Game')
+screen.title('My snake Game')
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
+
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle('square')
     new_segment.color('white')
     new_segment.goto(position)
+    segments.append(new_segment)
 
-screen.exitonclick()
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
