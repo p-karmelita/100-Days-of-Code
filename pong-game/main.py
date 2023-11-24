9e2a5f6cbf8aa054650f17124258ffb78b3a6785
Piotr Karmelita <piotr@karmelita.eu>
2023-11-24 23:35:43 +0100

    create ball class

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,11 +1,13 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
+ball = Ball()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
