831b098b8fab75c454e1b1280c0634c0f643460d
Piotr Karmelita <piotr@karmelita.eu>
2023-11-25 12:19:42 +0100

    update scoreboard

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
@@ -30,5 +32,13 @@ while game_is_on:
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
