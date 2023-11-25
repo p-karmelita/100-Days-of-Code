0735ac1fbbccbd146ef98e06281f7e55aee5ec15
Piotr Karmelita <piotr@karmelita.eu>
2023-11-25 12:31:59 +0100

    fix

--- pong-game/ball.py
+++ pong-game/ball.py
@@ -4,11 +4,12 @@ from turtle import Turtle
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.color('white')
+        self.color('red')
         self.shape('circle')
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -20,7 +21,9 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.bounce_x()
--- pong-game/paddle.py
+++ pong-game/paddle.py
@@ -5,7 +5,7 @@ class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
         self.shape('square')
-        self.color('white')
+        self.color('green')
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
