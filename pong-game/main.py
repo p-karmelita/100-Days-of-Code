79069f0cc3f86abb31fedf3b7647d6ddd68ef304
Piotr Karmelita <piotr@karmelita.eu>
2023-11-25 11:50:30 +0100

    fix

--- pong-game/main.py
+++ pong-game/main.py
@@ -14,8 +14,8 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(r_paddle.go_up(), 'Up')
-screen.onkey(r_paddle.go_down(), 'Down')
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
 screen.onkey(l_paddle.go_up, 'w')
 screen.onkey(l_paddle.go_down, 's')
 
@@ -26,6 +26,9 @@ while game_is_on:
     ball.move()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 screen.exitonclick()
