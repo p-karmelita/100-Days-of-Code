13707494fe3b9defe458db453b0f8ab6c6636a2e
Piotr Karmelita <piotr@karmelita.eu>
2023-11-29 13:01:22 +0100

    fix

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,13 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+FONT = ("Courier", 15, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.write(f'Level: {self.level}', align='left', font=FONT)
