88a39fda2586ba7a45e4033699ceee1b45676e97
Piotr Karmelita <piotr@karmelita.eu>
2023-11-29 13:17:57 +0100

    Create scoreboard

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -9,5 +9,17 @@ class Scoreboard(Turtle):
         self.level = 1
         self.hideturtle()
         self.penup()
-        self.goto(-280, 260)
+        self.goto(-280, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.write(f'Level: {self.level}', align='left', font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f'GAME OVER', align='center', font=FONT)
