8a875f97ceed398aaafab04f9dd1e428ce872f48
Piotr Karmelita <piotr@karmelita.eu>
2023-11-30 23:49:45 +0100

    improve snake game

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -14,11 +14,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write('GAME OVER', align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
