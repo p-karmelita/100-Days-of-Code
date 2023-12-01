708cf680e673f64f8e0f4e3e39c569125cc3455f
Piotr Karmelita <piotr@karmelita.eu>
2023-12-01 19:38:15 +0100

    fix

--- snake_game/main.py
+++ snake_game/main.py
@@ -33,12 +33,14 @@ while game_is_on:
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
             scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
