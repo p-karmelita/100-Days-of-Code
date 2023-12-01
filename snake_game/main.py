6159ed8755e88b2521e37a4b7d30f3ac390bae0d
Piotr Karmelita <piotr@karmelita.eu>
2023-12-01 19:28:16 +0100

    Change scoreboard.

--- snake_game/main.py
+++ snake_game/main.py
@@ -32,13 +32,12 @@ while game_is_on:
         scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
+        scoreboard.reset()
 
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_is_on = False
             scoreboard.reset()
 
 
