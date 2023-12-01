55f9df4ecf7eb15db70223fbeb8fee26faed9d9b
Piotr Karmelita <piotr@karmelita.eu>
2023-12-01 19:22:49 +0100

    fix

--- snake_game/main.py
+++ snake_game/main.py
@@ -38,6 +38,7 @@ while game_is_on:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
+            game_is_on = False
             scoreboard.reset()
 
 
