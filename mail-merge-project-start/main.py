ac245384f5d79d58ff902f4cbaa6f08209eca196
Piotr Karmelita <piotr@karmelita.eu>
2023-12-07 21:55:46 +0100

    iterate over names with invite letter

--- mail-merge-project-start/main.py
+++ mail-merge-project-start/main.py
@@ -1,3 +1,12 @@
+PLACEHOLDER = '[name]'
+
+
 with open('./Input/Names/invited_names.txt') as names_file:
-    names = names_file.read()
+    names = names_file.readlines()
     print(names)
+
+with open('./Input/Letters/starting_letter.txt') as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        print(new_letter)
